55e4c53f70fde0636624ecc3d340559bb40de506
Evan Ogas <evanogas@gmail.com>
2022-12-06 22:44:37 -0600

    top three elves

--- AdventOfCode01/Program.cs
+++ AdventOfCode01/Program.cs
@@ -15,8 +15,11 @@ foreach (var line in allLines)
     currentElf.FoodItems.Add(int.Parse(line));
 }
 
-var maxCalories = elves.Max(e => e.FoodItems.Sum());
-Console.Write(maxCalories);
+var topThreeCalories = elves
+    .OrderByDescending(e => e.FoodItems.Sum())
+    .Take(3)
+    .Sum(e => e.FoodItems.Sum());
+Console.Write(topThreeCalories);
 
 public record Elf
 {

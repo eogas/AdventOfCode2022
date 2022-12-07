b6964adba1e089c4dfe120ca9ff56f09f4ee3063
Evan Ogas <evanogas@gmail.com>
2022-12-06 22:36:28 -0600

    initial commit

---
+++ AdventOfCode01/Program.cs
@@ -0,0 +1,24 @@
+﻿var allLines = File.ReadAllLines("input.txt");
+
+var elves = new List<Elf>();
+var currentElf = new Elf();
+
+foreach (var line in allLines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        elves.Add(currentElf);
+        currentElf = new();
+        continue;
+    }
+
+    currentElf.FoodItems.Add(int.Parse(line));
+}
+
+var maxCalories = elves.Max(e => e.FoodItems.Sum());
+Console.Write(maxCalories);
+
+public record Elf
+{
+    public List<int> FoodItems = new();
+}
